10978137fb459334e61ae6b99bacb3fd111d12a5
gabriieljunior <153875095+gabriieljunior@users.noreply.github.com>
2024-12-09 22:26:31 -0300

    Alteração feitas nas rules do webpack pro css, criação do style da página.

--- bundle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var express = require('express');
-var app = express();
-app.listen(3000, function () {
-  console.log('Acessar http://localhost:3000');
-  console.log('Servidor executando na porta 3000.');
-});
